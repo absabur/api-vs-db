05b87906b1b2d8feac43e3dcb0cc88822bb7be1e
absabur <absabur929@gmail.com>
2025-08-01 19:33:28 +0600

    prefetch off

--- app/page.js
+++ app/page.js
@@ -31,6 +31,7 @@ export default function Home() {
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/direct-db"
             rel="noopener noreferrer"
+            prefetch={false}
           >
             Dynamic SSR Using DB
           </Link>
@@ -38,6 +39,7 @@ export default function Home() {
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto"
             href="/fetch-api"
             rel="noopener noreferrer"
+            prefetch={false}
           >
             Dynamic SSR Using Api
           </Link>
@@ -45,6 +47,7 @@ export default function Home() {
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="/csr"
             rel="noopener noreferrer"
+            prefetch={false}
           >
             Client Side Rendering
           </Link>
